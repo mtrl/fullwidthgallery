ca7ddd50099f0511a0e4b048e7637b625698cad9
Mark Williams <m.williams@contentbloom.com>
2014-07-02 17:21:48 +0100

    Added some responsive code

--- httpdocs/assets/js/rosemary.js
+++ httpdocs/assets/js/rosemary.js
@@ -1,12 +1,30 @@
-var imagesWide = 5;
+var imagesWide;
     
 $(window).load(function() {
     $(".fancybox").fancybox();
     thumbnailsFillScreen();
+    $(window).resize(function(){
+       thumbnailsFillScreen();
+    });
 });
+
+// Determine the number of images wide in order to optimise viewing on mobile devices
+function setNumberOfImagesWide() {
+    var screenWidth = $(window).width();
+    if (screenWidth <= 640) {
+        imagesWide = 2;
+    } else if (screenWidth <= 960) {
+        imagesWide = 3;
+    } else if (screenWidth <= 1178) {
+        imagesWide = 4;
+    }else {
+        imagesWide = 5;
+    }
+}
 
 // Fit thumbnails images to screen width
 function thumbnailsFillScreen() {
+    setNumberOfImagesWide();
     var images = $('#container');
     var itemCount = images.find('.item').length;
     for(i = imagesWide; i <= itemCount; i = i + imagesWide) {
