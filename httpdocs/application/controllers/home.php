8aaa41fa033a463d389aab9d6b5e6a0089c39875
Mark Williams <m.williams@contentbloom.com>
2014-07-02 08:31:48 +0100

    Added cache to ignore and enabled caching on homepage

--- httpdocs/application/controllers/home.php
+++ httpdocs/application/controllers/home.php
@@ -19,6 +19,8 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->output->cache(1440);
+		
 		$this->load->model('Photos');
 		$this->Photos->get_photos();
 		$this->load->view('home', array('photos' => $this->Photos->photos_by_month));
