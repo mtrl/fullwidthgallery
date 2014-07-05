52e3105fc0921937221f2095b1f939425bd66585
Mark Williams <m.williams@contentbloom.com>
2014-07-05 18:18:44 +0100

    Moved application and system out of httpdocs

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -56,7 +56,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'system';
+	$system_path = '/Users/mark/www/rosemary/system';
 
 /*
  *---------------------------------------------------------------
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'application';
+	$application_folder = '/Users/mark/www/rosemary/application';
 
 /*
  * --------------------------------------------------------------------
