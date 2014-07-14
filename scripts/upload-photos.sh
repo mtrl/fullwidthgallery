5de07f01b4a57f964d62c88028638351a425f23b
Mark Williams <m.williams@contentbloom.com>
2014-07-14 11:09:27 +0100

    Exclude 350 from upload

--- scripts/upload-photos.sh
+++ scripts/upload-photos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avz httpdocs/photos/ mark@mtrl.co.uk:/var/www/vhosts/rosemary.mtrl.co.uk/httpdocs/photos/ --exclude=1200 --exclude=200 --exclude=225 --progress
+rsync -avz httpdocs/photos/ mark@mtrl.co.uk:/var/www/vhosts/rosemary.mtrl.co.uk/httpdocs/photos/ --exclude=1200 --exclude=200 --exclude=225 --exclude=350 --progress
