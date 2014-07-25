5c169adc13bf874c69513efe859e3004b2f3df90
Mark Williams <m.williams@contentbloom.com>
2014-07-25 17:39:06 +0100

    Update clear prod cache

--- scripts/clear-prod-cache.sh
+++ scripts/clear-prod-cache.sh
@@ -1,2 +1,2 @@
-#!/bin/zsh
-ssh mark@mtrl.co.uk:rm -rf /var/www/vhosts/rosemary.mtrl.co.uk/application/cache/*
+#!/bin/bash
+ssh mark@mtrl.co.uk 'rm -rf /var/www/vhosts/rosemary.mtrl.co.uk/application/cache/*'
